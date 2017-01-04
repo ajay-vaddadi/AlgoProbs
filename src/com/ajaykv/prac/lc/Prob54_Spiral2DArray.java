324115f1d9975d24dbd5c4c17399518d7d5bb0b3
Ajay Vaddadi <ajaykv@groupon.com>
2017-01-03 16:40:18 -0800

    Leetcode Format

--- src/com/ajaykv/prac/lc/Prob54_Spiral2DArray.java
+++ src/com/ajaykv/prac/lc/Prob54_Spiral2DArray.java
@@ -1,67 +1,72 @@
 package com.ajaykv.prac.lc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prob54_Spiral2DArray {
 
     public static void main(String[] args) {
-        //declarations
-        int arrHeight = 4;
-        int arrWidth = 1;
-        int[][] arr = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
+        int[][] matrix = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
         Prob54_Spiral2DArray spiral = new Prob54_Spiral2DArray();
-        spiral.traverseClockWise(arr, arrHeight, arrWidth);
+        System.out.println(spiral.spiralOrder(matrix));
     }
 
-    public void traverseClockWise(int [][]arr, int arrHeight,int arrWidth){
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> finalList = new ArrayList<>();
+
+        if (matrix == null) { return finalList;}
+        if (matrix.length == 0) { return finalList;}
 
         int top = 0;
-        int bottom = arrHeight-1;
+        int bottom = matrix.length-1;
         int left = 0;
-        int right = arrWidth-1;
-
-        int i=0; int j=0;
+        int right = matrix[0].length-1;
 
         while (top <= bottom && left <= right) {
-//            System.out.println("Top: " + top + " Bottom:" + bottom + " Left:" + left + " Right:" + right);
 
-            traverseRight(arr, left, right, top);
+            traverseRight(matrix, left, right, top, finalList);
             top++;
             if(top>bottom) break;
 
-            traverseDown(arr, top, bottom, right);
+            traverseDown(matrix, top, bottom, right, finalList);
             right--;
             if (left > right) break;
 
-            traverseLeft(arr, right, left, bottom);
+            traverseLeft(matrix, right, left, bottom, finalList);
             bottom--;
             if(top>bottom) break;
 
-            traverseUp(arr, bottom, top, left);
+            traverseUp(matrix, bottom, top, left, finalList);
             left++;
             if (left > right) break;
         }
+
+        return finalList;
+
     }
 
-    private void traverseRight(int [][]arr, int left, int right, int top){
+    private void traverseRight(int [][]matrix, int left, int right, int top, List<Integer> finalList){
         for (int i = left;i<=right;i++){
-            System.out.println(arr[top][i]);
+            finalList.add(matrix[top][i]);
         }
     }
 
-    private void traverseDown(int [][]arr, int top, int bottom, int right){
+    private void traverseDown(int [][]matrix, int top, int bottom, int right, List<Integer> finalList){
         for (int i = top;i<=bottom;i++){
-            System.out.println(arr[i][right]);
+            finalList.add(matrix[i][right]);
         }
     }
 
-    private void traverseLeft(int [][]arr, int right, int left, int bottom){
+    private void traverseLeft(int [][]matrix, int right, int left, int bottom, List<Integer> finalList){
         for (int i = right;i>=left;i--){
-            System.out.println(arr[bottom][i]);
+            finalList.add(matrix[bottom][i]);
         }
     }
 
-    private void traverseUp(int [][]arr, int bottom, int top, int left){
+    private void traverseUp(int [][]matrix, int bottom, int top, int left, List<Integer> finalList){
         for (int i = bottom;i>=top;i--){
-            System.out.println(arr[i][left]);
+            finalList.add(matrix[i][left]);
         }
     }
 }
